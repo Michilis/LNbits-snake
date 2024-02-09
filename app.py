67de6102c4ef37b33c906f435cefb096f7e8b576
Michilis <120072772+Michilis@users.noreply.github.com>
2024-02-09 16:08:35 +0100

    Update app.py
    Change port

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Accessible on http://<server_ip>:5000
+    app.run(host='0.0.0.0', port=5055)  # Accessible on http://<server_ip>:5000
